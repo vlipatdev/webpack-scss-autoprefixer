4c8ababe96f220617769bfe9daafb6630133bd16
vlipatdev <vlipat.dev@gmail.com>
2019-06-27 09:43:45 +0800

    Add default sas

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    //remove on prod
     watch: true,
     entry: './src/js/index.js',
     output: {
